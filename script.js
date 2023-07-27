3247e77f8b29791687f9bb4c4c946f3b0800329a
Simon Mwangi <mpsimon2015@gmail.com>
2023-07-27 06:10:46 +0300

    Block addition of duplicate content

--- script.js
+++ script.js
@@ -30,6 +30,11 @@ function onAddItemSubmit(e) {
         itemToEdit.classList.remove('edit-mode');
         itemToEdit.remove();
         isEditMode = false;
+    } else {
+        if(checkIfItemExists(newItem)) {
+            alert('Item already exists!');
+            return;
+        }
     }
     
     // Create item DOM element
@@ -100,6 +105,11 @@ function onClickItem(e) {
         setItemToEdit(e.target);
     }
     
+}
+
+function checkIfItemExists(item) {
+    const itemsFromStorage = getItemsFromStorage();
+    return itemsFromStorage.includes(item);
 }
 
 function setItemToEdit(item) {
